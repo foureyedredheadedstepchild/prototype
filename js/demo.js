b7f2c9bb9e1d6ebf6c4768ab8557935a6118b9cc
Nala Nedwob <nedwob@live.com>
2017-10-19 17:20:22 -0300

    Dreamweaver CC 2018 commit...

--- js/demo.js
+++ js/demo.js
@@ -1,8 +1,9 @@
-(function(){
-  var parallax = document.querySelectorAll(".parallax"), speed = 0.5;
-  window.onscroll = function(){
-    [].slice.call(parallax).forEach(function(el,i){
-      var windowYOffset = window.pageYOffset, elBackgrounPos = "0 " + (windowYOffset * speed) + "px";
+(function() {
+  var parallax = document.querySelectorAll(".section-parallax"), speed = 0.5;
+  window.onscroll = function() {
+    [].slice.call(parallax).forEach(function(el, i) {
+      
+	  var windowYOffset = window.pageYOffset - 2823 / 2, elementOffset = el.offsetTop, elBackgrounPos = "50% " + (windowYOffset - elementOffset) * speed+ "px";
       el.style.backgroundPosition = elBackgrounPos;
     });
   };
