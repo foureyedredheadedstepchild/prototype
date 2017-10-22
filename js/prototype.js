ed486cc9f80d918d00c8467940a8c3fad767bbab
Nala Nedwob <nedwob@live.com>
2017-10-22 00:52:29 -0300

    fixes up the ass...

--- js/prototype.js
+++ js/prototype.js
@@ -1,10 +1,1 @@
-(function() {
-  var parallax = document.querySelectorAll(".section-parallax"), speed = 0.5;
-  window.onscroll = function() {
-    [].slice.call(parallax).forEach(function(el, i) {
-      
-	  var windowYOffset = window.pageYOffset, elementOffset = el.offsetTop / 2, elBackgrounPos = "50% -" + (windowYOffset) * speed + "px";
-      el.style.backgroundPosition = elBackgrounPos;
-    });
-  };
-})();
+;(function($){'use strict';$.fn.parallax=function(options){var settings=$.extend({speed:0.5},options);var windowHeight=$(window).height();return this.each(function(){var $this=$(this);$(document).scroll(function(){var scrollTop=$(window).scrollTop();var offset=$this.offset().top;var height=$this.outerHeight();if (offset+height<=scrollTop||offset>=scrollTop+windowHeight){return;}var y=Math.round((offset-scrollTop)*settings.speed);$this.css('transform','translateY('+y+'px)');});});};})(jQuery);
