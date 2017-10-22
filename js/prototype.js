09a91300b208d91bb666dabc540665fa7f2ddb20
Nala Nedwob <nedwob@live.com>
2017-10-22 17:42:27 -0300

    hopefully this is better...

--- js/prototype.js
+++ js/prototype.js
@@ -1,1 +1,22 @@
-;(function($){'use strict';$.fn.parallax=function(options){var settings=$.extend({speed:0.5},options);var windowHeight=$(window).height();return this.each(function(){var $this=$(this);$(document).scroll(function(){var scrollTop=$(window).scrollTop();var offset=$this.offset().top;var height=$this.outerHeight();if (offset+height<=scrollTop||offset>=scrollTop+windowHeight){return;}var y=Math.round((offset-scrollTop)*settings.speed);$this.css('transform','translateY('+y+'px)');});});};})(jQuery);
+;(function($) {
+	'use strict';
+	$.fn.parallax = function(options) {
+		var settings = $.extend({
+			speed: 0.15
+		}, options);		
+		var windowHeight = $(window).height();		
+		return this.each(function() {
+			var $this = $(this);			
+			$(document).scroll(function() {
+				var scrollTop = $(window).scrollTop();
+				var offset = $this.offset().top;
+				var height = $this.outerHeight();			
+				if (offset + height <= scrollTop || offset >= scrollTop + windowHeight) {
+					return;
+				}
+				var y = Math.round((offset - scrollTop) * settings.speed);
+				$this.css('transform', 'translateY(' + y + 'px)'); 
+			});
+		});
+	};
+})(jQuery);
