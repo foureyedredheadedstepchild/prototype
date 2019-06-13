75506d0977c252da0c8422b12ab56fffa1f4fca4
Nala Nedwob <nedwob@live.com>
2019-06-13 01:36:06 -0300

    ...

--- js/prototype.js
+++ js/prototype.js
@@ -1,38 +1,52 @@
 ;(function($) {
 	'use strict';
+	$.fn.isInViewport = function() {
+	  var offsetTop = $(this).offset().top;
+	  var outerHeight = offsetTop + $(this).outerHeight();
+	  var scrollTop = $(window).scrollTop();
+	  var height = scrollTop + $(window).height();
+	  return outerHeight > scrollTop && offsetTop < height;
+	};
+})(jQuery);
+
+;(function($) {
+	'use strict';
 	$.fn.parallax = function(options) {
-	    var $w = $(window);	
-		var settings = $.extend({}, $.fn.parallax.defaults, options);
-//		var settings = $.extend({ speed: 0.1 }, options);			
-		var windowHeight = $w.height();	
-	    $w.resize(function() {
-		    windowHeight = $w.height();
+		var defaults = $.extend({}, $.fn.parallax.defaults, options);
+//		var defaults = $.extend({ depth: 0.1 }, options);
+		var $window = $(window);	
+		var windowHeight = $window.height();	
+	    $window.resize(function() {
+		    windowHeight = $window.height();
+/*			
+            console.log('%c' + windowHeight, "background: red; 
+			                                  color: white; 
+											  padding-left:10px;");
+*/
 	    });	
 		return this.each(function() {
-			var $this = $(this);				
+			var $this = $(this);		
+			
 			$(document).scroll(function() {
-				var scrollTop = $w.scrollTop();
-				var offset = $this.offset().top;
-				var height = $this.outerHeight();	
-				if (offset + height <= scrollTop 
-					|| offset >= scrollTop + windowHeight) {
+				// vertical scrollbar position...
+				var scrollTop = $window.scrollTop();
+				var offset = parseInt($this.offset().top);
+				var height = $this.outerHeight(true);	
+/*	
+				if (offset + height <= scrollTop || offset >= scrollTop + windowHeight) {
 					return;
 				}
-				var y = Math.round((offset - scrollTop) * settings.speed);
-				$this.css('transform', 'translateY(' + -y + 'px)'); 		
-//              $this.css('background-position', 'center ' + y + 'px');
+*/				
+			    var visible = $this.isInViewport();
+				
+				if (visible) {
+					var y = Math.round((offset - scrollTop) * defaults.depth);
+					$this.css('transform', 'translateY(' + y + 'px)');
+				}
 			});
 		});
 	};
 	$.fn.parallax.defaults = {
-		speed : 0.1
+		depth : 0
 	};
 })(jQuery);
-
-/*
-	$.fn.parallax = function(s) {
-		var scrollTop = $(window).scrollTop();
-		var y = Math.round(scrollTop * s);
-		this.css('background-position', '0 -'+ y + 'px');
-	};
-*/
